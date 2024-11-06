defb4f721a6c84e91cf4ab99a44bb6157a1da5cd
bobems1215 <rgn5011@psu.edu>
2024-11-06 15:31:12 -0500

    Update psuedo_Miseq_creation.sh

--- psuedo_Miseq_creation.sh
+++ psuedo_Miseq_creation.sh
@@ -15,10 +15,17 @@
 #This is needed to load the usearch package 
 export PATH="$PATH:/storage/group/exd44/default/rgn5011/.local/bin/"
 
-#We want to make a new directory for the creation of Pseudo Miseq Reads
+#Note that raw reads can be downloaded in the rarify.reads.sh script
 
-mkdir ./NCBI_files/Psuedo_Miseq
-cd ./NCBI_files/Psuedo_Miseq
+#We want to make a new directory for the creation of Pseudo Miseq Reads
+#cd ./NCBI_files/GyrB_Sequencing_data
+
+cd /storage/group/exd44/default/rgn5011/NCBI_files/no_path_test/test_down/GyrB_Sequencing_data
+
+#Note if this file does not exist make sure to run the rarify.reads.sh script to download the raw data from the NCBI
+
+mkdir Psuedo_Miseq
+cd Psuedo_Miseq
 
 
 #We need to create a primer files of each of the orginal Moeller Primer sets.
@@ -41,70 +48,73 @@ echo '>Prim1R'>> Moeller_lac_primer.txt
 echo 'TRTANGAATCRTTRTGCTGC'>> Moeller_lac_primer.txt
 
 #We need to make folders for each of the famillies of interest 
-mkdir Psuedo_Bac
-mkdir Psuedo_Bif
-mkdir Psudo_Lac
+mkdir Bac
+mkdir Bif
+mkdir Lac
 
-mv Moeller_bac_primer.txt ./Psuedo_Bac
-mv Moeller_bif_primer.txt ./Psuedo_Bif
-mv Moeller_lac_primer.txt ./Psuedo_Lac
+mv Moeller_bac_primer.txt ./Bac
+mv Moeller_bif_primer.txt ./Bif
+mv Moeller_lac_primer.txt ./Lac
 
 #Start with Bacteroidaceae
-cd ./Psuedo_Bac
+cd ./Bac
 
-#I am assuming all data is a folder called Pacbio_data in the Directory NCBI_Scripts
+usearch -search_pcr ../../raw/GM_Bac_pac_1.fastq  -db ./Moeller_bac_primer.txt -strand both -maxdiffs 3 -minamp 30 -maxamp 2000  
+-ampoutq Bac_GM_pac.cut.fastq
 
-usearch -search_pcr ../../Pacbio_data/Bac_H1_pac.fastq -db ./Moeller_bac_primer.txt -strand both -maxdiffs 3 -minamp 30 -maxamp 2000  -ampoutq Bac_H1_pac.cut.fastq
+usearch -search_pcr ../../raw/H1_Bac_pac_1.fastq  -db ./Moeller_bac_primer.txt -strand both -maxdiffs 3 -minamp 30 -maxamp 2000  
+-ampoutq Bac_H1_pac.cut.fastq
+usearch -search_pcr ../../raw/H2_Bac_pac_1.fastq  -db ./Moeller_bac_primer.txt -strand both -maxdiffs 3 -minamp 30 -maxamp 2000  -ampoutq Bac_H2_pac.cut.fastq
 
-usearch -search_pcr ../../Pacbio_data/Bac_H2_pac.fastq -db ./Moeller_bac_primer.txt -strand both -maxdiffs 3 -minamp 30 -maxamp 2000  -ampoutq Bac_H2_pac.cut.fastq
+usearch -search_pcr ../../raw/H3_Bac_pac_1.fastq -db ./Moeller_bac_primer.txt -strand both -maxdiffs 3 -minamp 30 -maxamp 2000  -ampoutq Bac_H3_pac.cut.fastq
 
-usearch -search_pcr ../../Pacbio_data/Bac_H3_pac.fastq -db ./Moeller_bac_primer.txt -strand both -maxdiffs 3 -minamp 30 -maxamp 2000  -ampoutq Bac_H3_pac.cut.fastq
+usearch -search_pcr ../../raw/H4_Bac_pac_1.fastqq -db ./Moeller_bac_primer.txt -strand both -maxdiffs 3 -minamp 30 -maxamp 2000  -ampoutq Bac_H4_pac.cut.fastq
 
-usearch -search_pcr ../../Pacbio_data/Bac_H4_pac.fastq -db ./Moeller_bac_primer.txt -strand both -maxdiffs 3 -minamp 30 -maxamp 2000  -ampoutq Bac_H4_pac.cut.fastq
+usearch -search_pcr ../../raw/H5_Bac_pac_1.fastq -db ./Moeller_bac_primer.txt -strand both -maxdiffs 3 -minamp 30 -maxamp 2000  -ampoutq Bac_H5_pac.cut.fastq
 
-usearch -search_pcr ../../Pacbio_data/Bac_H5_pac.fastq -db ./Moeller_bac_primer.txt -strand both -maxdiffs 3 -minamp 30 -maxamp 2000  -ampoutq Bac_H5_pac.cut.fastq
+usearch -search_pcr ../../raw/M1_Bac_pac_1.fastq -db ./Moeller_bac_primer.txt -strand both -maxdiffs 3 -minamp 30 -maxamp 2000  -ampoutq Bac_M1_pac.cut.fastq
 
-usearch -search_pcr ../../Pacbio_data/Bac_M1_pac.fastq -db ./Moeller_bac_primer.txt -strand both -maxdiffs 3 -minamp 30 -maxamp 2000  -ampoutq Bac_M1_pac.cut.fastq
-
-usearch -search_pcr ../../Pacbio_data/Bac_M2_pac.fastq -db ./Moeller_bac_primer.txt -strand both -maxdiffs 3 -minamp 30 -maxamp 2000  -ampoutq Bac_M2_pac.cut.fastq
+usearch -search_pcr ../../raw/M2_Bac_pac_1.fastq -db ./Moeller_bac_primer.txt -strand both -maxdiffs 3 -minamp 30 -maxamp 2000  -ampoutq Bac_M2_pac.cut.fastq
 
 #Move to Bifidobacteriaceae
-cd ../Psuedo_Bif
+cd ../Bif
 
 #I am assuming all data is a folder called Pacbio_data in the Directory NCBI_Scripts
 
-usearch -search_pcr ../../Pacbio_data/Bif_H1_pac.fastq -db ./Moeller_bac_primer.txt -strand both -maxdiffs 3 -minamp 30 -maxamp 2000  -ampoutq Bif_H1_pac.cut.fastq
+usearch -search_pcr ../../raw/GM_Bif_pac_1.fastq -db ./Moeller_bif_primer.txt -strand both -maxdiffs 3 -minamp 30 -maxamp 2000  -ampoutq Bif_GM_pac.cut.fastq
 
-usearch -search_pcr ../../Pacbio_data/Bif_H2_pac.fastq -db ./Moeller_bac_primer.txt -strand both -maxdiffs 3 -minamp 30 -maxamp 2000  -ampoutq Bif_H2_pac.cut.fastq
+usearch -search_pcr ../../raw/H1_Bif_pac_1.fastq -db ./Moeller_bif_primer.txt -strand both -maxdiffs 3 -minamp 30 -maxamp 2000  -ampoutq Bif_H1_pac.cut.fastq
 
-usearch -search_pcr ../../Pacbio_data/Bif_H3_pac.fastq -db ./Moeller_bac_primer.txt -strand both -maxdiffs 3 -minamp 30 -maxamp 2000  -ampoutq Bif_H3_pac.cut.fastq
+usearch -search_pcr ../../raw/H2_Bif_pac_1.fastq -db ./Moeller_bif_primer.txt -strand both -maxdiffs 3 -minamp 30 -maxamp 2000  -ampoutq Bif_H2_pac.cut.fastq
 
-usearch -search_pcr ../../Pacbio_data/Bif_H4_pac.fastq -db ./Moeller_bac_primer.txt -strand both -maxdiffs 3 -minamp 30 -maxamp 2000  -ampoutq Bif_H4_pac.cut.fastq
+usearch -search_pcr ../../raw/H3_Bif_pac_1.fastq -db ./Moeller_bif_primer.txt -strand both -maxdiffs 3 -minamp 30 -maxamp 2000  -ampoutq Bif_H3_pac.cut.fastq
 
-usearch -search_pcr ../../Pacbio_data/Bif_H5_pac.fastq -db ./Moeller_bac_primer.txt -strand both -maxdiffs 3 -minamp 30 -maxamp 2000  -ampoutq Bif_H5_pac.cut.fastq
+usearch -search_pcr ../../raw/H4_Bif_pac_1.fastq -db ./Moeller_bif_primer.txt -strand both -maxdiffs 3 -minamp 30 -maxamp 2000  -ampoutq Bif_H4_pac.cut.fastq
 
-usearch -search_pcr ../../Pacbio_data/Bif_M1_pac.fastq -db ./Moeller_bac_primer.txt -strand both -maxdiffs 3 -minamp 30 -maxamp 2000  -ampoutq Bif_M1_pac.cut.fastq
+usearch -search_pcr ../../raw/H5_Bif_pac_1.fastq -db ./Moeller_bif_primer.txt -strand both -maxdiffs 3 -minamp 30 -maxamp 2000  -ampoutq Bif_H5_pac.cut.fastq
 
-usearch -search_pcr ../../Pacbio_data/Bif_M2_pac.fastq -db ./Moeller_bac_primer.txt -strand both -maxdiffs 3 -minamp 30 -maxamp 2000  -ampoutq Bif_M2_pac.cut.fastq
+usearch -search_pcr ../../raw/M1_Bif_pac_1.fastq -db ./Moeller_bif_primer.txt -strand both -maxdiffs 3 -minamp 30 -maxamp 2000  -ampoutq Bif_M1_pac.cut.fastq
+
+usearch -search_pcr ../../raw/M2_Bif_pac_1.fastq -db ./Moeller_bif_primer.txt -strand both -maxdiffs 3 -minamp 30 -maxamp 2000  -ampoutq Bif_M2_pac.cut.fastq
 
 
 #Move to Lachnospiraceae
-cd ../Psuedo_Lac
+cd ../Lac
 
 #I am assuming all data is a folder called Pacbio_data in the Directory NCBI_Scripts
 
-usearch -search_pcr ../../Pacbio_data/Lac_H1_pac.fastq -db ./Moeller_bac_primer.txt -strand both -maxdiffs 3 -minamp 30 -maxamp 2000  -ampoutq Lac_H1_pac.cut.fastq
+usearch -search_pcr ../../raw/GM_Lac_pac_1.fastq -db ./Moeller_lac_primer.txt -strand both -maxdiffs 3 -minamp 30 -maxamp 2000  -ampoutq Lac_GM_pac.cut.fastq
 
-usearch -search_pcr ../../Pacbio_data/Lac_H2_pac.fastq -db ./Moeller_bac_primer.txt -strand both -maxdiffs 3 -minamp 30 -maxamp 2000  -ampoutq Lc_H2_pac.cut.fastq
+usearch -search_pcr ../../raw/H1_Lac_pac_1.fastq -db ./Moeller_lac_primer.txt -strand both -maxdiffs 3 -minamp 30 -maxamp 2000  -ampoutq Lac_H1_pac.cut.fastq
 
-usearch -search_pcr ../../Pacbio_data/Lac_H3_pac.fastq -db ./Moeller_bac_primer.txt -strand both -maxdiffs 3 -minamp 30 -maxamp 2000  -ampoutq Lac_H3_pac.cut.fastq
+usearch -search_pcr ../../raw/H2_Lac_pac_1.fastq -db ./Moeller_lac_primer.txt -strand both -maxdiffs 3 -minamp 30 -maxamp 2000  -ampoutq Lc_H2_pac.cut.fastq
 
-usearch -search_pcr ../../Pacbio_data/Lac_H4_pac.fastq -db ./Moeller_bac_primer.txt -strand both -maxdiffs 3 -minamp 30 -maxamp 2000  -ampoutq Lac_H4_pac.cut.fastq
+usearch -search_pcr ../../raw/H3_Lac_pac_1.fastq -db ./Moeller_lac_primer.txt -strand both -maxdiffs 3 -minamp 30 -maxamp 2000  -ampoutq Lac_H3_pac.cut.fastq
 
-usearch -search_pcr ../../Pacbio_data/Lac_H5_pac.fastq -db ./Moeller_bac_primer.txt -strand both -maxdiffs 3 -minamp 30 -maxamp 2000  -ampoutq Lac_H5_pac.cut.fastq
+usearch -search_pcr ../../raw/H4_Lac_pac_1.fastq -db ./Moeller_lac_primer.txt -strand both -maxdiffs 3 -minamp 30 -maxamp 2000  -ampoutq Lac_H4_pac.cut.fastq
 
-usearch -search_pcr ../../Pacbio_data/Lac_M1_pac.fastq -db ./Moeller_bac_primer.txt -strand both -maxdiffs 3 -minamp 30 -maxamp 2000  -ampoutq Lac_M1_pac.cut.fastq
+usearch -search_pcr ../../raw/H5_Lac_pac_1.fastq -db ./Moeller_lac_primer.txt -strand both -maxdiffs 3 -minamp 30 -maxamp 2000  -ampoutq Lac_H5_pac.cut.fastq
 
-usearch -search_pcr ../../Pacbio_data/Lac_M2_pac.fastq -db ./Moeller_bac_primer.txt -strand both -maxdiffs 3 -minamp 30 -maxamp 2000  -ampoutq Lac_M2_pac.cut.fastq
+usearch -search_pcr ../../raw/M1_Lac_pac_1.fastq -db ./Moeller_lac_primer.txt -strand both -maxdiffs 3 -minamp 30 -maxamp 2000  -ampoutq Lac_M1_pac.cut.fastq
 
-
+usearch -search_pcr ../../raw/M2_Lac_pac_1.fastq -db ./Moeller_lac_primer.txt -strand both -maxdiffs 3 -minamp 30 -maxamp 2000  -ampoutq Lac_M2_pac.cut.fastq
